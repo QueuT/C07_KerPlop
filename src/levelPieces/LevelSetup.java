e2393443f196bacab246801509bffcc97293118c
Quincy Breaux <quincy_breaux@mines.edu>
2025-02-06 15:41:38 -0700

    Quincy, updated levelStage by implementing the two different cases for levels that will select different pieces for the board.

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -35,13 +35,54 @@ public class LevelSetup {
         // Use levelNum to differentiate the levels STILL NOT DONE!!!!!!!!!!!!!!
         switch (levelNum) {
             case 1:
-                setupLevelR();
+                setupLevelPieces1();
                 break;
             case 2:
-                setupLevelR();
+                setupLevelPieces2();
                 break;
         }
     }
+    //
+    
+    private void setupLevelPieces1() {
+    	
+    	Drawable background = new Drawable();
+    	
+    	Drawable enemy = new Moveable();
+    	Drawable trap = new Moveable();
+    	
+    	addRandomPiece(background);
+    	
+    	addRandomPiece(enemy);
+    	movingPieces.add(enemy);
+    	
+    	addRandomPiece(trap);
+    	movingPieces.add(trap);
+    	
+    }
+    
+    private void setupLevelPieces2() {
+    	
+    	Drawable background = new Drawable();
+    	
+    	Drawable enemy1 = new Moveable();
+    	Drawable enemy2 = new Moveable();
+
+    	Drawable obstacle = new Moveable();
+    	
+    	addRandomPiece(background);
+    	
+    	addRandomPiece(enemy1);
+    	movingPieces.add(enemy1);
+    	
+    	addRandomPiece(enemy2);
+    	movingPieces.add(enemy2);
+    	
+    	addRandomPiece(obstacle);
+    	movingPieces.add(obstacle);
+    	
+    }
+    
 
     // Method to add a piece to the board at a random location STILL NOT DONE!!!!!!!!!!!!!!
     private void addRandomPiece(Drawable piece) {
