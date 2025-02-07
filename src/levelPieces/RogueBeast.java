335f49bf2706feca447c466320034ffbc63b7778
Francez Fernandez <francez_fernandez@mines.edu>
2025-02-06 22:04:41 -0700

    Fernandez, created RogueBeast class that extends GamePiece and implements Moveable.

---
+++ src/levelPieces/RogueBeast.java
@@ -0,0 +1,34 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import gameEngine.Moveable;
+
+import java.util.Random;
+
+public class RogueBeast extends GamePiece implements Moveable {
+    private Random random;
+    
+	public RogueBeast(int location) {
+        super('R', "RogueBeast", location);
+        this.random = new Random();
+	}
+
+    @Override
+    public void move(Drawable[] gameBoard, int playerLocation) {
+        int randomMove = random.nextInt(2);
+        int newLocation = (randomMove == 0) ? getLocation() - 1 : getLocation() + 1;
+
+        if (newLocation >= 0 && newLocation < GameEngine.BOARD_SIZE) {
+            setLocation(newLocation);  // Update location
+        }
+    }
+
+    public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+        if (getLocation() == playerLocation) {
+            return InteractionResult.HIT;  // Damages the player when collided
+        }
+        return InteractionResult.NONE;
+    }
+}
