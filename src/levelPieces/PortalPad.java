51a73102b55882d06490cf7d7622e6601afa0fda
Quincy Breaux <quincy_breaux@mines.edu>
2025-02-06 21:00:03 -0700

    Changed PortalPad to only be Drawable not extending game piece.

---
+++ src/levelPieces/PortalPad.java
@@ -0,0 +1,15 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+
+public class PortalPad implements Drawable {
+
+
+    @Override
+    public void draw() {
+        System.out.print("O");  // PortalPad on the board
+    }
+
+}
