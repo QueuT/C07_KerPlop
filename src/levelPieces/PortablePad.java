74c64cea5171288c19113fc42667252726c5f5e1
Francez Fernandez <francez_fernandez@mines.edu>
2025-02-06 19:13:06 -0700

    Fernandez, added PortablePad as a non-iteracting Drawable piece.

--- src/levelPieces/PortablePad.java
+++ src/levelPieces/PortablePad.java
@@ -1,5 +1,26 @@
-package src.levelPieces;
+package levelPieces;
 
-public class PortablePad {
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
 
+public class PortalPad extends GamePiece implements Drawable {
+
+    public PortalPad(int location) {
+        super('w', "Doormat", location);  // 'w' is the symbol for PortalPad
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    public char getSymbol() {
+        return 'w';  // PortalPad symbol
+    }
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		return InteractionResult.NONE;
+	}
 }
