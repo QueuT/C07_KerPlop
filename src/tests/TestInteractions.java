c5220bd0a09dd1ff79c6600247e6dc687b08721f
Francez18 <francez_fernandez@mines.edu>
2025-02-10 21:38:36 -0700

    Updated TestInteraction.java

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -13,13 +13,15 @@ public class TestInteractions {
     @Test
     public void testGhostArcherInteraction() {
         Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
-        GhostArcher archer = new GhostArcher(8);
-        gameBoard[8] = archer;
+        GhostArcher archer = new GhostArcher(5);
+        gameBoard[8] = ghostArcher;
 
         // Test if GhostArcher interacts with player at distance
-        assertEquals(InteractionResult.KILL, archer.interact(gameBoard, 10), "GhostArcher should hit the player at position 10");
+        int playerLocation = 6; // Within range of the GhostArcher (attack range is 1)
+        assertEquals(InteractionResult.KILL, archer.interact(gameBoard, 10), "GhostArcher should kill the player when within range");
 
         // Test if GhostArcher does NOT interact with player at far distance
-        assertEquals(InteractionResult.NONE, archer.interact(gameBoard, 18), "GhostArcher should not hit the player at position 18");
+        playerLocation = 8; // Outside range
+        assertEquals(InteractionResult.NONE, archer.interact(gameBoard, 18), "GhostArcher should not interact with the player when out of range");
     }
 }
