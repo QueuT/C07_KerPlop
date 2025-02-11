87a837218a70dc3e58edb26ebb19a474a674e75b
Francez18 <francez_fernandez@mines.edu>
2025-02-10 20:31:51 -0700

    Fernandez, updated the TestInteractions.java but not sure if it is working.

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -1,5 +1,25 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import gameEngine.Drawable;
+import levelPieces.GhostArcher;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+
 public class TestInteractions {
 
+    // Test GhostArcher interaction with player
+    @Test
+    public void testGhostArcherInteraction() {
+        Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+        GhostArcher archer = new GhostArcher(8);
+        gameBoard[8] = archer;
+
+        // Test if GhostArcher interacts with player at distance
+        assertEquals(InteractionResult.HIT, archer.interact(gameBoard, 10), "GhostArcher should hit the player at position 10");
+
+        // Test if GhostArcher does NOT interact with player at far distance
+        assertEquals(InteractionResult.NONE, archer.interact(gameBoard, 18), "GhostArcher should not hit the player at position 18");
+    }
 }
